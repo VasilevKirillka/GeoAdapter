816edccf45b80435f831c5ce21fa6534bc4a3d75
Kirill <snamas@rambler.ru>
2023-11-09 17:15:16 +0700

    Создал сервис гео-адаптер, который извлекает нужные данные в классах GeoServiceCsv и GeoServiceHH и при помощи Kafka передает их в другой сервис.

--- src/main/java/geoadapter/controller/GeoController.java
+++ src/main/java/geoadapter/controller/GeoController.java
@@ -2,6 +2,7 @@ package geoadapter.controller;
 
 import geoadapter.dto.CountryDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,7 +14,7 @@ import geoadapter.services.GeoServiceHH;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Slf4j
 @RestController
 @RequestMapping("msg")
 @RequiredArgsConstructor
@@ -23,6 +24,8 @@ public class GeoController {
     private final GeoServiceCsv geoServiceCsv;
     @Value("${spring.kafka.topic}")
     private String geoTopic;
+    @Value("${myapp.data-source}")
+    private String dataSource;
 
 
     @PutMapping("/load")
@@ -35,11 +38,17 @@ public class GeoController {
 
     private List <CountryDto> addCountry(){
         List<CountryDto> countryDtoList =new ArrayList<>();
-        List<CountryDto> dtoListHh = geoServiceHH.hhGeoLoad();
-//        List<CountryDto> dtoListCsv = geoServiceCsv.csvGeoLoad();
-//        countryDtoList.addAll(dtoListCsv);
-        countryDtoList.addAll(dtoListHh);
-        return countryDtoList;
+        if ("hh".equals(dataSource)) {
+            countryDtoList.addAll(geoServiceHH.hhGeoLoad());
+        } else if ("csv".equals(dataSource)) {
+            countryDtoList.addAll(geoServiceCsv.csvGeoLoad());
+        } else if ("all".equals(dataSource)) {
+            countryDtoList.addAll(geoServiceCsv.csvGeoLoad());
+            countryDtoList.addAll(geoServiceHH.hhGeoLoad());
+        } else {
+            log.info("Выбран неверный ресурс");
+        }
 
+        return countryDtoList;
     }
 }
